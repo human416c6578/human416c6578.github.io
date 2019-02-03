a997beb68c5c89aa9cfa8546512b0accd1ef2b6f
human416c6578 <34635940+human416c6578@users.noreply.github.com>
2019-02-03 03:25:35 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-	define("DB_HOST", "host");
+	define("DB_HOST", "dr.laleagane.ro");
 	define("DB_USER", "root");
 	define("DB_PASS", "");
-	define("DB_NAME", "Timer");
+	define("DB_NAME", "Timer.db");
 
 	define("WEB_FILES_LINK", "http://");
 ?>
